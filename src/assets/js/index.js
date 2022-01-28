2002f68aa915e6b7e058947804efde9413a2236e
carlos-osorio-developer <osorio.uis@gmail.com>
2022-01-28 14:27:28 -0500

    Add basic JS to add new score

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import { newScore, scoresData } from "./api";
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();    
+  let userName = document.getElementById("name").value;
+  let userScore = document.getElementById("score").value;    
+  if (userName === '' || userScore === "") {
+    alert("Please fill out all fields");
+  } 
+  else {    
+    newScore(userName, userScore);    
+  }
+});
