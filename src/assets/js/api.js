2c014a6adb79b1fdc76b3d569ff3392fcbd0f808
carlos-osorio-developer <osorio.uis@gmail.com>
2022-01-28 19:48:50 -0500

    Fix linters errors

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,8 +1,8 @@
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 
 // Connect with API
 const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-let gameID = 'ClJ6JmiCgMSkp3eAuBNX';
+const gameID = 'ClJ6JmiCgMSkp3eAuBNX';
 
 // Create the new game connecting with the API
 // fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {method:'POST', headers: {'Content-Type': 'application/json',},body: JSON.stringify({name: 'SkullCrusher'})})
@@ -17,16 +17,16 @@ const scoresData = async () => {
   const response = await fetch(`${API_URL}/games/${gameID}/scores`);
   const data = await response.json();
   return data;
-}
+};
 
 const newScore = async (user, score) => {
   const response = await fetch(`${API_URL}/games/${gameID}/scores`, {
-    method:'POST', 
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({user: `${user}`, score: `${score}`})
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user: `${user}`, score: `${score}` }),
   });
   const data = await response.json();
   return data;
 };
 
-export {scoresData, newScore};
+export { scoresData, newScore };
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,42 +1,42 @@
-import { newScore, scoresData } from "./api";
+import { newScore, scoresData } from './api';
 import '../style/stylesheet.scss';
 
-const submitButton = document.getElementById("submit-button");
-const refreshButton = document.getElementById("refresh-button");
-const closeModalBtn = document.getElementById("close-modal-btn");
+const submitButton = document.getElementById('submit-button');
+const refreshButton = document.getElementById('refresh-button');
+const closeModalBtn = document.getElementById('close-modal-btn');
 
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();    
-  const userName = document.getElementById("name").value;
-  const userScore = document.getElementById("score").value;    
-  const modal = document.getElementById("error-modal");
-  if (userName === '' || userScore === "") {
-    modal.classList.remove("hidden");
-  } 
-  else {    
-    newScore(userName, userScore);    
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const userName = document.getElementById('name').value;
+  const userScore = document.getElementById('score').value;
+  const modal = document.getElementById('error-modal');
+  if (userName === '' || userScore === '') {
+    modal.classList.remove('hidden');
+  } else {
+    newScore(userName, userScore);
   }
 });
 
-const loadData = async() => {    
-  let scores = await scoresData();
-  const scoresList = document.getElementById("scores-list");
-  scoresList.innerHTML = "";
+const loadData = async () => {
+  const scores = await scoresData();
+  const scoresList = document.getElementById('scores-list');
+  scoresList.innerHTML = '';
   scores.result.sort((a, b) => b.score - a.score);
-  scores.result.forEach(score => scoresList.appendChild(createScoreListItem(score)));  
+
+  function createScoreListItem(score) {
+    const scoreListItem = document.createElement('tr');
+    scoreListItem.innerHTML = `<td>${score.user}</td><td>${score.score}</td>`;
+    return scoreListItem;
+  }
+
+  scores.result.forEach((score) => scoresList.appendChild(createScoreListItem(score)));
 };
 
-function createScoreListItem(score) {
-  const scoreListItem = document.createElement("tr");
-  scoreListItem.innerHTML = `<td>${score.user}</td><td>${score.score}</td>`;    
-  return scoreListItem;
-}
+refreshButton.addEventListener('click', loadData);
 
-refreshButton.addEventListener("click", loadData);
-
-closeModalBtn.addEventListener("click", () => {
-  const modal = document.getElementById("error-modal");
-  modal.classList.add("hidden");
+closeModalBtn.addEventListener('click', () => {
+  const modal = document.getElementById('error-modal');
+  modal.classList.add('hidden');
 });
 
 window.onload(loadData());
